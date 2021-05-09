4b09e6c90175736a8268df6d8824d4c318274fa8
vlad <vladkalyuzhnyu@gmail.com>
2021-05-09 13:11:08 +0300

    fix errors

--- src/main/kotlin/SpineFile.Phaser.Types.GameObjects.Group.module_phaser.kt
+++ src/main/kotlin/SpineFile.Phaser.Types.GameObjects.Group.module_phaser.kt
@@ -1,21 +1,9 @@
 @file:Suppress("INTERFACE_WITH_SUPERCLASS", "OVERRIDING_FINAL_MEMBER", "RETURN_TYPE_MISMATCH_ON_OVERRIDE", "CONFLICTING_OVERLOADS")
 package Phaser.Types.GameObjects.Group
 
-import kotlin.js.*
-import org.khronos.webgl.*
-import org.w3c.dom.*
-import org.w3c.dom.events.*
-import org.w3c.dom.parsing.*
-import org.w3c.dom.svg.*
-import org.w3c.dom.url.*
-import org.w3c.fetch.*
-import org.w3c.files.*
-import org.w3c.notifications.*
-import org.w3c.performance.*
-import org.w3c.workers.*
-import org.w3c.xhr.*
 import Phaser.GameObjects.GameObject
 import Phaser.Types.Input.HitAreaCallback
+import ru.androidovshchik.InvalidNaming
 
 typealias GroupCallback = (item: GameObject) -> Unit
 
@@ -92,88 +80,110 @@ external interface GroupCreateConfig {
     var setXY: Any?
         get() = definedExternally
         set(value) = definedExternally
-    var setXY.x: Number?
+    @InvalidNaming
+    var setXY_x: Number?
         get() = definedExternally
         set(value) = definedExternally
-    var setXY.y: Number?
+    @InvalidNaming
+    var setXY_y: Number?
         get() = definedExternally
         set(value) = definedExternally
-    var setXY.stepX: Number?
+    @InvalidNaming
+    var setXY_stepX: Number?
         get() = definedExternally
         set(value) = definedExternally
-    var setXY.stepY: Number?
+    @InvalidNaming
+    var setXY_stepY: Number?
         get() = definedExternally
         set(value) = definedExternally
     var setRotation: Any?
         get() = definedExternally
         set(value) = definedExternally
-    var setRotation.value: Number?
+    @InvalidNaming
+    var setRotation_value: Number?
         get() = definedExternally
         set(value) = definedExternally
-    var setRotation.step: Number?
+    @InvalidNaming
+    var setRotation_step: Number?
         get() = definedExternally
         set(value) = definedExternally
     var setScale: Any?
         get() = definedExternally
         set(value) = definedExternally
-    var setScale.x: Number?
+    @InvalidNaming
+    var setScale_x: Number?
         get() = definedExternally
         set(value) = definedExternally
-    var setScale.y: Number?
+    @InvalidNaming
+    var setScale_y: Number?
         get() = definedExternally
         set(value) = definedExternally
-    var setScale.stepX: Number?
+    @InvalidNaming
+    var setScale_stepX: Number?
         get() = definedExternally
         set(value) = definedExternally
-    var setScale.stepY: Number?
+    @InvalidNaming
+    var setScale_stepY: Number?
         get() = definedExternally
         set(value) = definedExternally
     var setOrigin: Any?
         get() = definedExternally
         set(value) = definedExternally
-    var setOrigin.x: Number?
+    @InvalidNaming
+    var setOrigin_x: Number?
         get() = definedExternally
         set(value) = definedExternally
-    var setOrigin.y: Number?
+    @InvalidNaming
+    var setOrigin_y: Number?
         get() = definedExternally
         set(value) = definedExternally
-    var setOrigin.stepX: Number?
+    @InvalidNaming
+    var setOrigin_stepX: Number?
         get() = definedExternally
         set(value) = definedExternally
-    var setOrigin.stepY: Number?
+    @InvalidNaming
+    var setOrigin_stepY: Number?
         get() = definedExternally
         set(value) = definedExternally
     var setAlpha: Any?
         get() = definedExternally
         set(value) = definedExternally
-    var setAlpha.value: Number?
+    @InvalidNaming
+    var setAlpha_value: Number?
         get() = definedExternally
         set(value) = definedExternally
-    var setAlpha.step: Number?
+    @InvalidNaming
+    var setAlph_step: Number?
         get() = definedExternally
         set(value) = definedExternally
     var setDepth: Any?
         get() = definedExternally
         set(value) = definedExternally
-    var setDepth.value: Number?
+    @InvalidNaming
+    var setDepth_value: Number?
         get() = definedExternally
         set(value) = definedExternally
-    var setDepth.step: Number?
+    @InvalidNaming
+    var setDepth_step: Number?
         get() = definedExternally
         set(value) = definedExternally
     var setScrollFactor: Any?
         get() = definedExternally
         set(value) = definedExternally
-    var setScrollFactor.x: Number?
+    @InvalidNaming
+    var setScrollFactor_x: Number?
         get() = definedExternally
         set(value) = definedExternally
-    var setScrollFactor.y: Number?
+    @InvalidNaming
+    var setScrollFactor_y: Number?
         get() = definedExternally
         set(value) = definedExternally
-    var setScrollFactor.stepX: Number?
+    @InvalidNaming
+    var setScrollFactor_stepX: Number?
         get() = definedExternally
         set(value) = definedExternally
-    var setScrollFactor.stepY: Number?
+    @InvalidNaming
+    var setScrollFactor_stepY: Number?
         get() = definedExternally
         set(value) = definedExternally
     var hitArea: Any?
